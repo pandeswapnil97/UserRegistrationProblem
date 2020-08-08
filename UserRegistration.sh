721527c1b254b8ede05f3ed2950b07fc44dbdf84
pandeswapnil97 <pandeswapnil97@gmail.com>
2020-08-08 15:38:44 +0530

    Validating Email Address

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -33,20 +33,42 @@ echo "Welcome to User Registration Problem"
 
 #Use Case 2 (Validating Last Name)
 
-read -p "Enter Last Name: " last_name
+#read -p "Enter Last Name: " last_name
+#
+#last_name_pattern="^[[:upper:]]{1,}"
+#
+#last_name_count=${#last_name}
+#
+#if [ $last_name_count -ge 3 ]
+#then
+#        if [[ $last_name =~ $last_name_pattern ]]
+#        then
+#                echo "Last name is valid"
+#        else
+#                echo "Last name is invalid"
+#        fi
+#else
+#        echo "Entered last name does not contain minimum 3 characters"
+#fi
 
-last_name_pattern="^[[:upper:]]{1,}"
 
-last_name_count=${#last_name}
 
-if [ $last_name_count -ge 3 ]
+
+
+
+
+
+#Use Case 3 (Validating Entered Email Address)
+
+read -p "Enter Email Address: " email_address
+
+email_address_pattern="(^[a-zA-Z0-9]+)([.]{0,})([a-zA-Z0-9]+)([@]{1})([a-zA-Z]+)([.]{0,1})([a-zA-Z]{0,4})([.]{0,1})([a-z]{0,2})"
+
+#email_address_pattern="^([a-zA-Z0-9]+)([.]{0,1})([a-zA-Z0-9]{0,})@([a-zA-Z0-9]+)([.]{0,})([a-zA-Z]+)([.]{0,1})({a-zA-Z}{0,})$"
+
+if [[ $email_address =~ $email_address_pattern ]]
 then
-        if [[ $last_name =~ $last_name_pattern ]]
-        then
-                echo "Last name is valid"
-        else
-                echo "Last name is invalid"
-        fi
+       echo "Email address is valid"
 else
-        echo "Entered last name does not contain minimum 3 characters"
+       echo "Email address is not valid"
 fi
